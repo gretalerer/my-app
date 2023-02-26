ae637de0bdb7179965549a5a77ded404ee1d9cd9
Greta Lerer <gretalerer@Gretas-MacBook-Pro.local>
2023-02-26 19:58:14 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Helllo this is my first React App .
+       </h1>
+       <h1> I am learning React for the Forage virtual internship</h1>
+
     </div>
   );
 }
